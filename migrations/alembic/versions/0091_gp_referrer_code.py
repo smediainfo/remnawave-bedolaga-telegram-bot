30e21369893566c9b35234b8fd10cd6fd563d6c2
smediainfo <dev@matrixvpn.top>
2026-06-03 13:05:37 +0300

    fix(migrations): renumber custom 0086-0088 -> 0089-0091 after upstream v3.59.0 tail (0088 dedupe_tariff)

--- migrations/alembic/versions/0091_gp_referrer_code.py
+++ migrations/alembic/versions/0091_gp_referrer_code.py
@@ -14,8 +14,8 @@ from typing import Sequence, Union
 import sqlalchemy as sa
 from alembic import op
 
-revision: str = '0088'
-down_revision: Union[str, None] = '0087'
+revision: str = '0091'
+down_revision: Union[str, None] = '0090'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
